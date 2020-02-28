e8664ee8216aa805bf87c3c92a9fbf93fcff95d1
Daspe Etienne <etienne.daspe@gmail.com>
2020-02-28 15:00:04 +0100

    Update comments.

--- src/utils/confusionMatrix.go
+++ src/utils/confusionMatrix.go
@@ -2,6 +2,28 @@ package utils
 
 import "fmt"
 
+// ConfusionMatrix shows the performance of the algorithm.
+//
+// Example with k=7:
+//
+//             | Actual class
+//             |_________________________________________
+//             | 0   1   2   3   4   5   6   7   8   9
+// ____________|_________________________________________
+//           0 | 87| 0 | 0 | 0 | 1 | 0 | 0 | 0 | 0 | 0  |
+//           1 | 0 | 65| 1 | 1 | 1 | 0 | 1 | 1 | 11| 10 |
+//           2 | 1 | 0 | 73| 5 | 0 | 0 | 0 | 0 | 2 | 5  |
+//           3 | 0 | 0 | 0 | 68| 0 | 2 | 0 | 1 | 9 | 11 |
+// Predicted 4 | 0 | 0 | 0 | 0 | 83| 0 | 0 | 0 | 0 | 9  |
+// class     5 | 0 | 0 | 0 | 0 | 0 | 72| 9 | 0 | 0 | 10 |
+//           6 | 0 | 0 | 0 | 0 | 0 | 0 | 91| 0 | 0 | 0  |
+//           7 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 83| 1 | 5  |
+//           8 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 1 | 78| 9  |
+//           9 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 92 |
+//
+// More information:
+// https://en.wikipedia.org/wiki/Confusion_matrix
+//
 type ConfusionMatrix struct {
 	TestData    Samples
 	Predictions Samples
